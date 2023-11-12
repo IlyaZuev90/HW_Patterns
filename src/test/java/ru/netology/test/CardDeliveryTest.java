cd4d41435f77dd26c6e4e0e1885b47730a966846
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-11-12 23:46:59 +0400

    Added solution to the first task

---
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -0,0 +1,52 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.data.DataUserGenerator;
+import java.time.Duration;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+    @BeforeEach
+    public void setupAll() {
+        open("http://localhost:9999"); }
+
+    @Test
+    public void shouldTestScheduleMeeting() {
+
+        DataUserGenerator.UserInfo validUser = DataUserGenerator.Registration.generateUser("ru");
+        int addDaysToFirstMeeting = 3;
+        String firstDateOfTheMeeting = DataUserGenerator.generateDate(addDaysToFirstMeeting);
+        int addDaysToSecondMeeting = 7;
+        String secondDateOfTheMeeting = DataUserGenerator.generateDate(addDaysToSecondMeeting);
+
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(firstDateOfTheMeeting);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(byText("Запланировать")).click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstDateOfTheMeeting))
+                .shouldBe(visible);
+
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(secondDateOfTheMeeting);
+        $(byText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(visible);
+
+        $("[data-test-id='replan-notification'] button").click();
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondDateOfTheMeeting))
+                .shouldBe(visible);
+
+    }
+}
